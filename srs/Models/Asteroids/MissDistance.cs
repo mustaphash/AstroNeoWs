174e2689832a236f67bdb3c0cf546e5fc086598c
mustaphash <79004067+mustaphash@users.noreply.github.com>
2021-05-31 14:49:45 +0300

    Add files via upload

---
+++ srs/Models/Asteroids/MissDistance.cs
@@ -0,0 +1,13 @@
+﻿namespace Models.Asteroids
+{
+    public class MissDistance
+    {
+        public string astronomical { get; set; }
+
+        public string lunar { get; set; }
+
+        public string kilometers { get; set; }
+
+        public string miles { get; set; }
+    }
+}
